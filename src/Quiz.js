39eff39f1cb5081093580468c776bea13e10b4c5
sagandhi95 <sohamgandhi95@gmail.com>
2018-12-09 23:18:42 +0530

    Module 5 completed

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,6 +12,12 @@ class Quiz extends Component {
         };
     }
 
+    showNextQuestion() {
+        this.setState((prevState) => ({
+            quiz_position: prevState.quiz_position + 1
+        }));
+    };
+
     render(){
 
         const isQuizEnd = (this.state.quiz_position - 1) === quizData.quiz_questions.length;
@@ -24,7 +30,8 @@ class Quiz extends Component {
                         :
                         <QuizQuestion 
                             className="QuizQuestion" 
-                            quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} 
+                            quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
+                            showNextQuestionHandler={this.showNextQuestion.bind(this)} 
                         />
                 }
             </div>
